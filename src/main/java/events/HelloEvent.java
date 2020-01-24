0cfcd92ab2946f79d3acc2ec63d2583474274baa
rzhang57 <48890532+rzhang57@users.noreply.github.com>
2020-01-24 15:31:23 -0800

    ok

--- src/main/java/events/HelloEvent.java
+++ src/main/java/events/HelloEvent.java
@@ -1,6 +1,7 @@
 package events;
 
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -9,8 +10,11 @@ public class HelloEvent extends ListenerAdapter {
 
     public void onGuildMessageReceived (GuildMessageReceivedEvent event){
         String messageSent = event.getMessage().getContentRaw();
+
+        String user = event.getAuthor().getName();
+
         if(messageSent.equalsIgnoreCase("Hello!")){
-            event.getChannel().sendMessage("Hello").queue();
+            event.getChannel().sendMessage("Hello " + user + "! To start, please type '!help' for a list of commands.").queue();
         }
 
     }
